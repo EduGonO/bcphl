2ffb0e9d1012c3eae8268453cd5868bd5fb9308e
edu gonzalez <46654892+EduGonO@users.noreply.github.com>
2025-02-16 12:13:48 +0100

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
-import Header from '../components/Header';
+import Header from '../Header';
 
 export type Article = {
   title: string;
